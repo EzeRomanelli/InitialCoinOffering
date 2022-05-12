927c04b28e0743a6257c955fb294f27529b33035
Eze Romanelli <ezeromanelli@MacBook-Pro-de-Eze.local>
2022-05-12 13:39:46 -0300

    First Commit

---
+++ hardhat/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x7917A70EE4EFAe890c0fb9E91136d8F15Bfcc976";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
